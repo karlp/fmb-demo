6150fdec519a42c61527e17fcd53fe197b2c94af
Karl Palsson <karlp@tweak.net.au>
2015-04-25 03:05:17 +0000

    makefiles: disable asserts for smaller code
    assert is pulling in printf

--- rules.mk
+++ rules.mk
@@ -32,6 +32,7 @@ CFLAGS += -Wredundant-decls -Wstrict-prototypes
 #CFLAGS += -Wmissing-prototypes
 CFLAGS += -ffunction-sections -fdata-sections
 CFLAGS += $(OPENCM3_DEFS)
+CFLAGS += -DNDEBUG # squelch asserts in freemodbus lib
 
 LDSCRIPT = $(OPENCM3_DIR)/lib/stm32/l1/stm32l15xxb.ld
 
