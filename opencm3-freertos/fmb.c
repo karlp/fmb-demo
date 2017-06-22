4f5c4ea90d985ea82653938d3b5a1caf51d753d8
Karl Palsson <karlp@etactica.com>
2017-06-22 13:43:43 +0000

    just enable all the ports :)
    just a simple hack right now to make it simpler to edit syscfg.h

--- opencm3-freertos/fmb.c
+++ opencm3-freertos/fmb.c
@@ -34,6 +34,7 @@ static void gpio_setup(void)
 {
 	/* blinken lights */
 	rcc_periph_clock_enable(RCC_GPIOB);
+	rcc_periph_clock_enable(RCC_GPIOA);
 	gpio_mode_setup(LED_BLUE_PORT, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, LED_BLUE_PIN);
 #if defined(LED_GREEN_PORT)
 	gpio_mode_setup(LED_GREEN_PORT, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, LED_GREEN_PIN);
